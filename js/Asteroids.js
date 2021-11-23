eb9fa60efb446ed947696d1a6f5c0a3677cb5515
Paulo Rodrigues <paulofilipe_100pt@hotmail.com>
2021-11-23 11:24:10 +0000

    Segunda Entrega

--- js/Asteroids.js
+++ js/Asteroids.js
@@ -8,8 +8,6 @@ canvas.height = window.innerHeight - 10;
 const W = canvas.width;
 const H = canvas.height;
 
-ctx.fillStyle = 'Blue'; //eliminar depois
-
 //setas
 let upKey = false;
 let downKey = false;
@@ -41,70 +39,67 @@ let asteroides = [];
 //array de tiros
 let tiros = [];
 
+//ovni
+let ovni;
+
 //array da nave
 let nave = {
     x: W / 2,
     y: H / 2,
     w: 80,
     h: 75,
-    imagem: imagens['Nave'],
-    colicaoX: 12,
-    colicaoY: 5,
-    colicaoW: 25,
-    colicaoH: 10,
+    imagem: imagens['Nave'], 
     vida: true,
     vidas: 5,
+    angulo: 0,
+    colisao: {  //correção para as coordenadas de colisao
+        x: -30,
+        y: -30,
+        w: -20,
+        h: -10
+    }
 };
+
 
 //classes
 //asteroides
 class Asteroides{
     constructor(x, y, d, img){
-        this.x = x;
-        this.y = y;
-        this.dX = 2 * Math.cos(d);
-        this.dY = 2 * Math.sin(d);
-        this.img = img;
-        this.w = img.width;
-        this.h = img.height;
-        this.partir = false;
+        this.x = x; //posisão do asteroide em x
+        this.y = y; //posisão do asteroide em y
+        this.dX = 2 * Math.cos(d); //direção do asteroide em x
+        this.dY = 2 * Math.sin(d); //direção do asteroide em y
+        this.img = img; //imagem do asteroide
+        this.w = img.width; //largura
+        this.h = img.height; //altura
     }
 
     draw(){
-        if(!this.partir){
-            //desenhar os asteroides
-            ctx.fillStyle = 'blue' //ponto de colição (mudar/remover depois)
-            ctx.beginPath();
-            ctx.fillRect(this.x + 5, this.y + 5, this.w - 10, this.h - 10); //ponto de colição (mudar/remover depois)
-            ctx.drawImage(this.img, this.x, this.y); 
-        }          
+        //desenhar os asteroides
+        ctx.drawImage(this.img, this.x, this.y);         
     }
 
     update(){
-        if(!this.partir){
-            // atualizar a posição dos asteroides
-            if (this.x < -120) this.x = W
-            if (this.x > W) this.x = -120
-            if (this.y < -68)  this.y = H
-            if (this.y > H) this.y = -68
+        // atualizar a posição dos asteroides
+        if (this.x < -120) this.x = W
+        if (this.x > W) this.x = -120
+        if (this.y < -68)  this.y = H
+        if (this.y > H) this.y = -68
 
-            this.x += this.dX;
-            this.y += this.dY;
-        }
-        
-        this.partir = colisoesNave(this.x + 5, this.y + 5, imagens['Meteoro 2'].width - 10, imagens['Meteoro 2'].height - 10)  //verificar se um asteroide bateu na nave
+        this.x += this.dX;
+        this.y += this.dY;
     }
 }
 
 //tiros
 class Tiro{
     constructor(x, y, c, t, xDir, yDir){
-        this.x = x;
-        this.y = y;
-        this.c = c;
-        this.t = t;
-        this.dX = xDir;
-        this.dY = yDir;
+        this.x = x; //posisão do tiro em x
+        this.y = y; //posisão do tiro em y
+        this.c = c; //cor do tiro
+        this.t = t; //tamanho do tiro
+        this.dX = 2 * Math.cos(xDir); //direção do tiro em x
+        this.dY = 2* Math.sin(yDir); //direção do tiro em y        
     }
 
     draw(){
@@ -116,15 +111,45 @@ class Tiro{
 
     update(){
         //atualizar a posição do tiro
-        if(this.dX < this.x) this.x -= 5;
-        if (this.dX > this.x) this.x += 5;
-        if (this.dY < this.y) this.y -= 5;
-        if (this.dY > this.y) this.y += 5;
+        // if(this.dX < this.x) this.x -= 5;
+        // if (this.dX > this.x) this.x += 5;
+        // if (this.dY < this.y) this.y -= 5;
+        // if (this.dY > this.y) this.y += 5;
+
+        if (this.x < 0) this.x = W 
+        if (this.x > W) this.x = 0
+        if (this.y < 0)  this.y = H
+        if (this.y > H) this.y = 0
+
+        this.x += this.dX;
+        this.y += this.dY;
+        
         
         //retirar o tiro quando chegar ao destino
         if (this.xDir == this.x && this.yDir == this.y) {
             tiros.shift();
         }
+    }
+}
+
+//ovni
+class OVNI{
+    constructor(x, y, d){
+        this.x = x; 
+        this.y = y; 
+        this.d = d;
+        this.img = imagens['Ovni'];
+    }
+
+    draw(){
+        //desenhar o OVNI
+        ctx.drawImage(this.img, this.x, this.y);  
+    }
+
+    update(){
+        // atualizar a posição dos ovni
+        this.x += this.dX;
+        this.y += this.dY;
     }
 }
 
@@ -139,9 +164,6 @@ function loadImage(name){
 function KeyPressed(e){
     if (e.key == 'w'){
         upKey = true;
-    }
-    if (e.key == 's'){
-        downKey = true;
     }
     if (e.key == 'a'){
         leftKey = true;
@@ -162,9 +184,6 @@ function KeyReleased(e){
         case "w":
             upKey = false; ;
             break;
-        case "s":
-            downKey = false;
-            break;
         case "a":
             leftKey = false;
             break;
@@ -179,8 +198,8 @@ function KeyReleased(e){
 
 //teleportar a nave
 function teleport(){
-    nave.x = Math.floor(Math.random() * (canvas.width ));
-    nave.y = Math.floor(Math.random() * (canvas.height));
+    nave.x = Math.floor(Math.random() * W);
+    nave.y = Math.floor(Math.random() * H);
 }
 
 //tempo de espera para poder teleportar novamente
@@ -192,27 +211,45 @@ function teleportTimer(){
     }
 }
 
-//colisões
-function colisoesNave(x, y, w, h){
-    if (x + w < (nave.x + nave.colicaoX) || x > (nave.x + nave.colicaoX) + (nave.w - nave.colicaoW)|| y + h < (nave.y + nave.colicaoY) || y > (nave.y + nave.colicaoY) + (nave.h - nave.colicaoH)) {
-        //sem colições
-    } 
-    else {
-        nave.vida = false;  //dar a informação que a nave foi destruida
-        return true;
-        }
-}
-
-function colisoes(obj1, obj2){
-    if ((Math.floor(obj1.x) + 5) + (obj1.w - 10) < obj2.x || 
-        (Math.floor(obj1.x) + 5) > obj2.x + obj2.w ||
-        (Math.floor(obj1.y) + 5) + (obj1.h - 10) < obj2.y ||
-        (Math.floor(obj1.y) + 5) > obj2.y + obj2.h) {
+/**
+ * colisões
+ * @param {object} obj1 asteroides 
+ * @param {object} obj2 tiros ou nave
+ * @param {object} nave se a nave
+ * @returns 
+ */
+function colisoes(obj1, obj2, nave = false){
+    if (nave){
+        if ((Math.floor(obj1.x)) + (obj1.w) < (obj2.x + obj2.colisao.x) || 
+        (Math.floor(obj1.x)) > (obj2.x + obj2.colisao.x) + (obj2.w + obj2.colisao.w) ||
+        (Math.floor(obj1.y)) + obj1.h < (obj2.y + obj2.colisao.y) ||
+        (Math.floor(obj1.y)) > (obj2.y + obj2.colisao.y) + (obj2.h + obj2.colisao.h)) {
         return false;
     } 
     else { 
         return true;
         }
+    }
+    else{
+        if ((Math.floor(obj1.x)) + (obj1.w) < obj2.x || 
+        (Math.floor(obj1.x)) > obj2.x + obj2.w ||
+        (Math.floor(obj1.y)) + (obj1.h) < obj2.y ||
+        (Math.floor(obj1.y)) > obj2.y + obj2.h) {
+        return false;
+    } 
+    else { 
+        return true;
+        }
+    }
+}
+
+//função para ajustar o angulo
+function angulo(){
+    if(nave.angulo >= 360){
+        nave.angulo = nave.angulo - 360;
+    }else if(nave.angulo <= -360){
+        nave.angulo = -360 - nave.angulo;
+    }
 }
 
 //render
@@ -221,30 +258,76 @@ function render(){
 
     //quando a nave tiver vidas
     if(nave.vida){
+        angulo() //repor o angulo da nave para 0 quando passar de +/-360
+
         //mover a nave
         if (upKey) {
-            nave.y -= 3;
-            if (nave.y < -nave.h){
-                nave.y = H;
+            if(nave.angulo == 0){ //frente
+                nave.y -= 3;
+                if (nave.y < -nave.h){
+                    nave.y = H;
+                }
+            }else if((nave.angulo<0 && nave.angulo>-90) || (nave.angulo< 360 && nave.angulo > 270)){ // diagonal superior esquerda
+                nave.y -= 3;
+                nave.x -= 3;
+                if (nave.y < -nave.h){
+                    nave.y = H;
+                }
+                if (nave.x < -nave.w){
+                    nave.x = W;
+                }
+            }else if((nave.angulo>0 && nave.angulo<90) || (nave.angulo> -360 && nave.angulo < -270)){ //diagonal superior direita
+                 nave.y -= 3;
+                 nave.x += 3;
+                 if (nave.y < -nave.h){
+                     nave.y = H;
+                 }
+                 if (nave.x > W){
+                    nave.x = -nave.w;
+                }
+             }else if(nave.angulo == -90 || nave.angulo == 270){ //esquerda
+                 nave.x -= 3;
+                 if (nave.x < -nave.w){
+                    nave.x = W;
+                }
+             }else if((nave.angulo < -90 && nave.angulo >- 180) || (nave.angulo < 270 && nave.angulo > 180)){ //diagonal inferior esquerda
+                nave.y += 3;
+                nave.x -= 3;
+                if (nave.x < -nave.w){
+                    nave.x = W;
+                }
+                if (nave.y > H){
+                    nave.y = -nave.h;
+                }
+            }else if(nave.angulo == 180 || nave.angulo == -180){ //baixo
+                nave.y += 3;
+                if (nave.y > H){
+                    nave.y = -nave.h;
+                }
+            }else if((nave.angulo>90 && nave.angulo<180) || (nave.angulo> -270 && nave.angulo < -180)){ //diagonal inferior direita
+                nave.y += 3;
+                nave.x += 3;
+                if (nave.y > H){
+                    nave.y = -nave.h;
+                }
+                if (nave.x > W){
+                    nave.x = -nave.w;
+                }
+            }else if(nave.angulo == 90 || nave.angulo == -270){ //direita
+                nave.x += 3;
+                if (nave.x > W){
+                    nave.x = -nave.w;
+                }
             }
         }
-        if (downKey) {
-            nave.y += 3;
-            if (nave.y > H){
-                nave.y = -nave.h;
-            }
-        }
+
         if (leftKey) {
-            nave.x -= 3;
-            if (nave.x < -nave.w){
-                nave.x = W;
-            }
+            //roda o angulo para a esquerda
+            nave.angulo --
         }
         if (rightKey) {
-            nave.x += 3;
-            if (nave.x > W){
-                nave.x = -nave.w;
-            }
+            //roda o angulo da nave para a direita
+            nave.angulo ++
         }
 
         //teleportar a nave
@@ -254,10 +337,11 @@ function render(){
         }
 
         //pintar a nave
-        ctx.beginPath();
-        ctx.fillStyle = 'blue' //ponto de colição (mudar/remover depois)
-        ctx.fillRect(nave.x + 12, nave.y + 5, nave.w - 25, nave.h - 10); //ponto de colição (mudar/remover depois)
-        ctx.drawImage(nave.imagem, nave.x, nave.y, nave.w, nave.h);
+        ctx.save()
+        ctx.translate(nave.x,nave.y)
+        ctx.rotate(nave.angulo*Math.PI/180)
+        ctx.drawImage(nave.imagem, -nave.w/2, -nave.h/2, nave.w, nave.h);
+        ctx.restore()
 
         //disparar com o click do rato
         if (click){
@@ -265,7 +349,7 @@ function render(){
             click = false;
         }
 
-        //tiro -> asteroides
+        //tiro -> asteroides (colisão)
         for (let t = 0; t < tiros.length; t++){
             for(let a = 0; a < asteroides.length; a++){
                 let colicao = colisoes(asteroides[a], tiros[t]);
@@ -275,6 +359,16 @@ function render(){
                     asteroides.splice(a, 1);
                     break
                 }
+            }
+        }
+
+        //nave -> asteroides (colisão)
+        for (let a = 0; a < asteroides.length; a++){
+            let colicao = colisoes(asteroides[a], nave, true);
+
+            if(colicao){ //quando a nave bate contra o asteroide
+                asteroides.splice(a, 1);
+                nave.vida = false;
             }
         }
     }
@@ -306,12 +400,28 @@ canvas.addEventListener('mousedown', (e) => {
 
 //criar asteroides 
 for (let i = 0; i < 10; i++) {
-    let xInit = 20 + Math.random() * (W - 2 * 20);
-    let yInit = 20 + Math.random() * (H - 2 * 20);
-
+    let xInit;
+    let yInit;
     let direction = Math.random() * 2 * Math.PI;
 
-    asteroides.push(new Asteroides(xInit, yInit, direction, imagens['Meteoro 2']))
+    //diferenciar a posição inical do asteroide dependedo da direção
+    if(direction < 1 || direction > 5){
+        xInit = Math.random() * W/4;
+        yInit = Math.random() * H;
+    }
+    else if(direction < 2) {
+        xInit = Math.random() * W;
+        yInit = Math.random() * H/4;
+    }
+    else if (direction < 4){
+        xInit = Math.random() * W + W * 3/4;
+        yInit = Math.random() * H;
+    }
+    else {
+        xInit = Math.random() * W;
+        yInit = Math.random() * H + H * 3/4;
+    }    
+    asteroides.push(new Asteroides(xInit, yInit, direction, imagens['Meteoro 2']));
 }
 
 window.onload = () => render()  //chamar a função render depois de carregar a pagina
