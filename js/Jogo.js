52958c5a3613adb69c755590dca69e5445c9dce5
Paulo Rodrigues <paulofilipe_100pt@hotmail.com>
2021-12-08 16:35:36 +0000

    Commit final
    Co-Authored-By: Sofia Freitas <72983751+SofiaFreitas711@users.noreply.github.com>

--- js/Jogo.js
+++ js/Jogo.js
@@ -29,30 +29,46 @@ let yR;
 
 //imagens
 let imagens = {}
-loadImage('Nave');
-loadImage("Meteoro 1");
-loadImage("Meteoro 2");
-loadImage("Meteoro 3");
-loadImage("Ovni");
+loadImageSVG('Nave');
+loadImagePNG("Meteoro 1");
+loadImagePNG("Meteoro 2");
+loadImagePNG("Meteoro 3");
+loadImageSVG("Ovni");
 
 //array de asteroides
 let asteroides = [];
 let numAsteroides = 5 //numero de asteroides
 
 //ovni
-let ovni = new OVNI(ctx, imagens['Ovni'], W, H);;
+let ovni = new OVNI(ctx, imagens['Ovni'], W, H);
+let tirOvni_timer
 
-//inivializar nave
+//inicializar nave
 let nave = new Nave(ctx, W, H, imagens['Nave']);
 
 //function
-//carregar imagens
-function loadImage(name){
-    imagens[name] = new Image();
-    imagens[name].src = 'img/' + name + '.png';
+/**
+ * carregar imagens .png
+ * @param {string} name nome da imagem
+ */
+function loadImagePNG(nome){
+    imagens[nome] = new Image();
+    imagens[nome].src = 'img/' + nome + '.png';
 }
 
-//clicar numa tecla
+/**
+ * carregar imagens .svg
+ * @param {string} name nome da imagem
+ */
+ function loadImageSVG(nome){
+    imagens[nome] = new Image();
+    imagens[nome].src = 'img/' + nome + '.svg';
+}
+
+/**
+ * clicar numa tecla
+ * @param {*} e tecla precionada
+ */
 function KeyPressed(e){
     if (e.key == 'w'){
         wKey = true;
@@ -75,7 +91,10 @@ function KeyPressed(e){
     }
 }
 
-//lagar a tecla
+/**
+ * lagar a tecla
+ * @param {*} e tecla que estava a ser precionada
+ */
 function KeyReleased(e){
     switch (e.key){
         case "w":
@@ -93,13 +112,17 @@ function KeyReleased(e){
     }
 }
 
-//teleportar a nave
+/**
+ * teletransportar a nave
+ */
 function teleport(){
     nave.x = Math.floor(Math.random() * W);
     nave.y = Math.floor(Math.random() * H);
 }
 
-//tempo de espera para poder teleportar novamente
+/**
+ * tempo de espera para poder teletransportar novamente
+ */
 function teleportTimer(){
     spaceTimer--;
     if (spaceTimer == 0){
@@ -109,8 +132,8 @@ function teleportTimer(){
 
 /**
  * colisões
- * @param {object} obj1 objeto de colição um 
- * @param {object} obj2 objeto de colição dois
+ * @param {object} obj1 objeto de colição 
+ * @param {object} obj2 objeto de colição
  * @returns 
  */
 function colisoes(obj1, obj2) {
@@ -118,40 +141,56 @@ function colisoes(obj1, obj2) {
     (obj1.x + obj1.colisao.x) > (obj2.x + obj2.colisao.x) + (obj2.w + obj2.colisao.w) ||
     (obj1.y + obj1.colisao.y) + (obj1.h + obj1.colisao.h) < (obj2.y + obj2.colisao.y) ||
     (obj1.y + obj1.colisao.y) > (obj2.y + obj2.colisao.y) + (obj2.h + obj2.colisao.h)) {
-    return false;
+    return false;   //objetos não colidiram
     } 
     else {
-        return true;
+        return true;    //objetos colidiram
     }
 }
 
+/**
+ * colisões
+ * @param {object} tiro
+ * @param {object} obj objeto de colição
+ * @returns 
+ */
 function colisaoTiros(tiro, obj){
     if (tiro.x + tiro.t < (obj.x + obj.colisao.x) || 
         tiro.x > (obj.x + obj.colisao.x) + (obj.w + obj.colisao.w) ||
         tiro.y + tiro.t < (obj.y + obj.colisao.y) ||
         tiro.y > (obj.y + obj.colisao.y) + (obj.h + obj.colisao.h)) {
-            return false;
+            return false;   //tiro não colidiu com nenhum objeto
         } 
         else { 
-            return true;
+            return true;    //tiro colidiu com objeto
         }
 }
 
-//criar ovni
+/**
+ * criar ovni
+ */
 function novOVNI(){
     if(!ovni.emJogo){
-        ovni.criarOVNI()
+        ovni.criarOVNI();
+        tirOvni_timer = window.setInterval(tirOVNI, 1000);
     }
 }
 
-//fazer com que o ovni dispara
+/**
+ * fazer com que o ovni dispara
+ */
 function tirOVNI(){
-    if(ovni.emJogo){
-        ovni.disparar(nave.x - nave.w/2, nave.y - nave.h/2)
+    if(ovni.emJogo && nave.vida){ //disparar quando o ovni e a nave estam no jogo
+        ovni.disparar(nave.x, nave.y)
+    }
+    else{ //limpar o temporizador quando o onvi sair do jogo
+        clearInterval(tirOvni_timer)
     }
 }
 
-//criar asteroides
+/**
+ * criar asteroides
+ */
 function criarAsteroides() {
     for (let i = 0; i < numAsteroides; i++) {
         let xInit;
@@ -182,11 +221,16 @@ function criarAsteroides() {
     numAsteroides += 2;
 }
 
+/**
+ * destruir/dividir asteroides 
+ * @param {int} index posição do asteroide no array
+ * @param {boolean} especial se o tiro da nave foi o tiro especial
+ */
 function destruirAsteroides(index, especial = false) {
     let x = asteroides[index].x;
     let y = asteroides[index].y;
     let estado = asteroides[index].estado;
-
+    
     if(estado < 3 && !especial){
         estado++
 
@@ -200,12 +244,15 @@ function destruirAsteroides(index, especial = false) {
     asteroides.splice(index, 1);
 }
 
+/**
+ * carregar o tiro especial
+ */
 function carregarEspecial(){
-    if(especialTimer == 0) {
-        especial = true;
+    if(especialTimer == 0) { 
+        especial = true; //ativa a possibilidade de disparar
     }
     else if(especialTimer > 0){
-        especialTimer--
+        especialTimer-- //diminui o tempo caso a variavel seja superior a 0
     }
 }
 
@@ -224,7 +271,7 @@ function render(){
             nave.desaceleracao()
         }
 
-        //teleportar a nave
+        //teletransportar a nave
         if (espacoKey){
             teleport()
             espacoKey = false;
@@ -291,16 +338,16 @@ function render(){
 
         //nave -> ovni (colisão)
         if(ovni.emJogo){
-            if(colisoes(ovni, nave, true)){ //quando a nave bate contra o OVNI
+            if(colisoes(ovni, nave)){ //quando a nave bate contra o OVNI
                 ovni.emJogo = false;
                 nave.perderVida();
             }
         }
 
-        // //OVNI -> asteroides (colisão)
+        //OVNI -> asteroides (colisão)
         for (let a = 0; a < asteroides.length; a++){
             if(ovni.emJogo){
-                if(colisoes(asteroides[a], ovni, true)){ //quando a nave bate contra o asteroide
+                if(colisoes(asteroides[a], ovni)){ //quando a nave bate contra o asteroide
                     destruirAsteroides(a)
                     
                     ovni.emJogo = false;
@@ -341,20 +388,21 @@ function render(){
 
         //quando o tiro passar o limite do canvas, passar para o outro lado
         if (tiro.y < -tiro.t){
-            tiro.y = H;
+            tiro.y = H; //aparece em baixo
         }
         if (tiro.y > H + tiro.t){
-            tiro.y = -tiro.t;
+            tiro.y = -tiro.t; //aparece em cima
         }
         if (tiro.x > W + tiro.t){
-            tiro.x = -tiro.t;
+            tiro.x = -tiro.t;   //aparece do lado esquerdo
         }
         if (tiro.x < -tiro.t){
-            tiro.x = W;
+            tiro.x = W; //aparece do lado direito
         }
         
     })
 
+    //desenhar e mover o ovni
     if(ovni.emJogo){
         ovni.draw();
         ovni.update();
@@ -401,22 +449,20 @@ function render(){
     ctx.stroke();
     ctx.fillText(especialTimer > 0 ? especialTimer : 'FIRE', W - 75, H - 115);
 
-
     window.requestAnimationFrame(render);
 }
 
 window.addEventListener('keydown', KeyPressed);
 window.addEventListener('keyup', KeyReleased);
 
-//receber a infoção do click do rato e das coordenadas
+//receber a informação do click do rato e das coordenadas
 canvas.addEventListener('mousedown', (e) => {
     click = true
     xR = e.clientX;
     yR = e.clientY;
 })
 
-setInterval(novOVNI, 25000);
-setInterval(tirOVNI, 1000);
-setInterval(carregarEspecial, 1000)
+setInterval(novOVNI, 25000); //criar um ovni 25 em 25 segundos
+setInterval(carregarEspecial, 1000) //carregar o tiro especial
 
 window.onload = () => render()  //chamar a função render depois de carregar a pagina
